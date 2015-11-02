4556cf99884d9e0ba0b0bee834c9a421f3a9312d
Gris Ge <fge@redhat.com>
2015-11-02 16:40:11 +0800

    Add _trim_tailling_space()
    Signed-off-by: Gris Ge <fge@redhat.com>

---
+++ string.c
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2015 Red Hat, Inc.
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author: Gris Ge <fge@redhat.com>
+ */
+
+static char *_trim_tailling_space(char *str);
+
+static char *_trim_tailling_space(char *str) {
+    char *end = NULL;
+    if ((str == NULL) || (*str == '\0'))
+        return;
+    end = str + strlen(str) - 1;
+    while(end > str && *end == ' ') --end;
+    *(end + 1) = 0;
+    return str;
+}
